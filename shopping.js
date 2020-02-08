4ff062c21e14431328664ae99e95af33f0a6e944
Maria Maria <maria.shulgina.fi@gmail.com>
2020-02-08 14:52:32 +0200

    commit

--- shopping.js
+++ shopping.js
@@ -1,24 +1,72 @@
-const express =require('express');
-const PORT = process.env.PORT ||8080;
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const body_parser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
-let app =express();
+//Controllers
+const auth_controller = require('./controllers/auth_controller');
+const note_controller = require('./controllers/note_controller');
 
-app.use ((req,res,next) =>{
-    console.log('PATH: '+ req.path);
+let app = express();
+
+app.use(body_parser.urlencoded({
+    extended: true
+}));
+
+app.use(session({
+    secret: '1234qwerty',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000000
+    }
+}));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
     next();
 });
 
-app.get('/', (req, res, next) =>{
+const is_logged_handler = (req, res, next) => {
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+    next();
+};
 
-    res.send('Hei');
-    res(end);
-});
+//Serve Static files
+app.use('/css', express.static('css'))
 
-app.use((req,res,next)=>{
-    console.log('404');
+//Auth
+app.use(auth_controller.handle_user);
+app.get('/login', auth_controller.get_login);
+app.post('/login', auth_controller.post_login);
+app.post('/register', auth_controller.post_register);
+app.post('/logout', auth_controller.post_logout);
+
+
+//Notes
+app.get('/', is_logged_handler, note_controller.get_notes);
+app.post('/delete-note', is_logged_handler, note_controller.post_delete_note);
+app.get('/note/:id', is_logged_handler, note_controller.get_note);
+app.post('/add-note', is_logged_handler, note_controller.post_note);
+
+app.use((req, res, next) => {
     res.status(404);
-    res.send('404');
-    res.end();
+    res.send(`
+        page not found
+    `);
 });
 
-app.listen(PORT);
+//Shutdown server CTRL + C in terminal
+const mongoose_url = 'mongodb+srv://mariaS:<password>@cluster0-ws0n9.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoose_url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Mongoose connected');
+    console.log('Start Express server');
+    app.listen(PORT);
+});
